4bea846c8dabb12141e5667ee8cf1b156985a12b
setuorangetoolz <setu@orangetoolz.com>
2024-04-25 12:19:21 +0600

    sum of number with reduce

--- SumOfNum.js
+++ SumOfNum.js
@@ -1,12 +1,19 @@
 const SumOfNum = (num) => {
-    //without converting string
-  let sum = 0;
-  for (;num>0; num =Math.floor(num/10)){
-      // console.log(num % 10+"nnnn")
-      sum += num % 10
-      // console.log(sum+"sss")
-  }
-  return sum
+
+    //with reduce
+    let NumArray = Array.from(String(num),Number);
+    return NumArray.reduce((accum,curr)=>accum+curr,0)
+
+
+
+  //   //without converting string
+  // let sum = 0;
+  // for (;num>0; num =Math.floor(num/10)){
+  //     // console.log(num % 10+"nnnn")
+  //     sum += num % 10
+  //     // console.log(sum+"sss")
+  // }
+  // return sum
 }
 
 console.log(SumOfNum(1234))
