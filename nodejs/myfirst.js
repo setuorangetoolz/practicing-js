395dabb4bd91d751f9afb6b4afd89a3ce4d19f15
setu <setu@orangetoolz.com>
2025-05-25 15:27:54 +0600

    Refactor HTML structure and implement StarPattern rendering in Star.html; update server response in myfirst.js to include current date and time.

--- nodejs/myfirst.js
+++ nodejs/myfirst.js
@@ -1,6 +1,9 @@
 const http = require('https');
+const dt = require("./myfirstModule")
 
 http.createServer((req,res)=>{
     res.writeHead(200,{'content-type':'text/html'});
-    res.end("hello world")
+    res.write("hello world"+dt.myDateTime())
+    res.end()
 }).listen(8080)
+
--- nodejs/myfirstModule.js
+++ nodejs/myfirstModule.js
@@ -1,10 +1,3 @@
-const http = require('https');
-
-http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/html'});
-    res.end("hello world")
-}).listen(8080)
-
 
 // create own
 exports.myDateTime = function (){
